a799d97ba540635a55eeeb6f3aa4828baf42b771
xirtampots <91842050+xirtampots@users.noreply.github.com>
2021-10-05 02:56:27 +0900

    Create 5585.cpp

---
+++ 5585.cpp
@@ -0,0 +1,36 @@
+//boj 5585
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    int n, answer = 0;
+    cin >> n;
+    n = 1000 - n;
+
+    if (n >= 500) {
+        answer = n / 500;
+        n %= 500;
+    }
+    if (n >= 100) {
+        answer += n / 100;
+        n %= 100;
+    }
+    if (n >= 50) {
+        answer += n / 50;
+        n %= 50;
+    }
+    if (n >= 10) {
+        answer += n / 10;
+        n %= 10;
+    }
+    if (n >= 5) {
+        answer += n / 5;
+        n %= 5;
+    }
+    answer += n;
+    cout << answer << '\n';
+}
